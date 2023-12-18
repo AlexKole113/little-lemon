162c6ae611e994fb6d140a10d4dcf61e0f6e99bd
AlexKole113 <alexander.koledov@gmail.com>
2023-12-18 12:51:26 +0100

    update mock settings

--- src/__mocks__/apiMock.js
+++ src/__mocks__/apiMock.js
@@ -3,7 +3,7 @@
 // so..
 
 const responseTimeModifier      = 2000;
-const successAppearanceModifier = .95;
+const successAppearanceModifier = .98;
 
 const seededRandom = seed => {
     const m = 2 ** 35 - 31;
